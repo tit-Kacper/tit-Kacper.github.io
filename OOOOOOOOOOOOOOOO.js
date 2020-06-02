3e4e4a5ee3c45ac8bd16ad0f087ecf5d3012fbdf
tit-Kacper <64528581+tit-Kacper@users.noreply.github.com>
2020-06-02 21:54:55 +0200

    Update OOOOOOOOOOOOOOOO.js

--- OOOOOOOOOOOOOOOO.js
+++ OOOOOOOOOOOOOOOO.js
@@ -1,10 +1,10 @@
 function zmien1()
 {
-	document.getElementById('foto1').src="radosnosc.png";
+	document.getElementById('foto1').src="radosnosc.jpg";
 }
 function zmien2()
 {
-	document.getElementById('foto1').src="radosnosc.png";
+	document.getElementById('foto1').src="radosnosc.jpg";
 }
 
     
