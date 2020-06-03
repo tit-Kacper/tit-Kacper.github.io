41062727233c755c83e44ae9325a654e1b0cfaab
tit-Kacper <64528581+tit-Kacper@users.noreply.github.com>
2020-06-03 13:13:39 +0200

    Update OOOOOOOOOOOOOOOO.js

--- OOOOOOOOOOOOOOOO.js
+++ OOOOOOOOOOOOOOOO.js
@@ -1,19 +1,32 @@
 function zmien1()
 {
-	document.getElementById('foto1').src="radosnosc.jpg";
+	document.getElementById('1').src="radosnosc.jpg";
 }
 function zmien2()
 {
-	document.getElementById('foto2').src="smutek.jpg";
+	document.getElementById('1').src="foto1.png";
 }
-function zmien3()
+function zmien3 ()
+{	
+		document.getElementById('2').src="smutek.jpg";
+}
+function zmien4 ()
 {
-	document.getElementById('foto3').src="szok.jpg";
+		document.getElementById('2').src="foto2.png";
 }
-function zmien4()
+function zmien5 ()
 {
-	document.getElementById('foto4').src="norbi.png";
+	document.getElementById('3').src="szok.jpg";
 }
-
-
-
+function zmien6 ()
+{
+	document.getElementById('3').src="foto3.jpg";
+}
+function zmien7 ()
+{
+	document.getElementById('4').src="norbi.png";
+}
+function zmien8 ()
+{
+	document.getElementById('4').src="foto4.png";
+}
